e32ddd37475245d93a3a9b2708593f7af4c797f6
Bhavani Krishna <kbkrishnacse@gmail.com>
2017-01-04 23:14:23 +1100

    html modules

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,8 @@ var inject = require('gulp-inject');
 var pkg = require('./package.json');
 var replace = require('gulp-replace');
 var removeEmptyLines = require('gulp-remove-empty-lines');
+var fs = require("fs");
+var compileHtmlModuleTemplate = fs.readFileSync('compile-module-template.html');
 
 // Set the banner content
 var banner = ['/*!\n',
@@ -90,50 +92,87 @@ gulp.task('minify-js-dashboard', function() {
 })
 
 // Compile Actions
-gulp.task('compile-actions', function() {
-    fs = require("fs");
-    var actions = fs.readdirSync('actions');
-    var template = fs.readFileSync('action-template.html');
-    actions.forEach(function(action){ 
-        fs.writeFileSync('actions/'+action+'/'+action+'-compiled.html', template);
-
-        gulp.src('actions/'+action+'/'+action+'-compiled.html')
-        .pipe(inject(gulp.src(['actions/'+action+'/'+action+'.less']).pipe(less()).pipe(cleanCSS({ compatibility: 'ie8' })), {
-            starttag: '<!-- inject:css -->',
-            transform: function (filePath, file) {
-                // return file contents as string 
-                return file.contents.toString('utf8')
-            }
-        }))
-        .pipe(inject(gulp.src(['actions/'+action+'/'+action+'.html']), {
-            starttag: '<!-- inject:html -->',
-            transform: function (filePath, file) {
-                // return file contents as string 
-                return file.contents.toString('utf8')
-            }
-        }))
-        .pipe(inject(gulp.src(['actions/'+action+'/'+action+'.js']).pipe(uglify()), {
-            starttag: '<!-- inject:js -->',
-            transform: function (filePath, file) {
-                // return file contents as string 
-                return file.contents.toString('utf8')
-            }
-        }))
-        .pipe(replace('<!-- inject:css -->', ''))
-        .pipe(replace('<!-- inject:js -->', ''))
-        .pipe(replace('<!-- inject:html -->', ''))
-        .pipe(replace('<!-- endinject -->', ''))
-        .pipe(removeEmptyLines({
-            removeComments: true
-        }))
-        .pipe(replace(new RegExp('<style>\n</style>', 'g'), ''))
-        .pipe(replace(new RegExp('<script type="text/javascript">\n</script>', 'g'), ''))
-        .pipe(removeEmptyLines({
-            removeComments: true
-         }))
-        .pipe(gulp.dest('actions/'+action));
-    });
+gulp.task('compile-html-modules', function() {
+    var basePath = 'templates';
+    var module_folders = ['pages','partials'];
+    var currentPath =  null;
+    var template_modules = fs.readdirSync(basePath);
+    template_modules.forEach(function(template_module_path){  
+        currentPath = basePath + '/' + template_module_path;
+        if (fs.statSync(currentPath).isDirectory()) {
+            template_module = fs.readdirSync(currentPath);
+            module_folders.forEach(function(module_folder){
+                currentPath = basePath + '/' + template_module_path + '/' + module_folder;
+                if (fs.statSync(currentPath).isDirectory()) {
+                    var possible_module_folder = fs.readdirSync(currentPath);
+                    possible_module_folder.forEach(function(possible_module){
+                        currentPath = basePath + '/' + template_module_path + '/' + module_folder + '/' + possible_module;
+                        if (fs.statSync(currentPath).isDirectory()) {
+                            gutil.log('compiling module', currentPath);   
+                            compileHtmlTemplateModule(possible_module, currentPath, (basePath + '/' + template_module_path + '/' + module_folder))                         
+                        }                                                
+                    }); 
+                } 
+            })            
+        }              
+    });    
 })
+
+function watchHtmlDirForModuleChanges(baseDirPath){
+    if (fs.statSync(baseDirPath).isDirectory()) {
+        var possible_module_folder = fs.readdirSync(baseDirPath);
+        possible_module_folder.forEach(function(possible_module){
+            var currentPath = baseDirPath + '/' + possible_module;
+            if (fs.statSync(currentPath).isDirectory()) {
+                gutil.log('..', currentPath)
+                gulp.watch(currentPath+'/**', function () {
+                    gutil.log('compiling module', currentPath)
+                    compileHtmlTemplateModule(possible_module, currentPath, baseDirPath)                         
+                    // gulp.run(browserSync.reload);
+                });
+            }
+        })
+    }
+}
+
+function compileHtmlTemplateModule(moduleName, dirPath, parentDirPath) {
+  fs.writeFileSync(dirPath + '.html', compileHtmlModuleTemplate);
+  gulp.src(dirPath + '.html')
+    .pipe(inject(gulp.src([dirPath+'/'+moduleName+'.less']).pipe(less()).pipe(cleanCSS({ compatibility: 'ie8' })), {
+        starttag: '<!-- inject:css -->',
+        transform: function (filePath, file) {
+            // return file contents as string 
+            return file.contents.toString('utf8')
+        }
+    }))
+    .pipe(inject(gulp.src([dirPath+'/'+moduleName+'.html']), {
+        starttag: '<!-- inject:html -->',
+        transform: function (filePath, file) {
+            // return file contents as string 
+            return file.contents.toString('utf8')
+        }
+    }))
+    .pipe(inject(gulp.src([dirPath+'/'+moduleName+'.js']).pipe(uglify()), {
+        starttag: '<!-- inject:js -->',
+        transform: function (filePath, file) {
+            // return file contents as string 
+            return file.contents.toString('utf8')
+        }
+    }))
+    .pipe(replace('<!-- inject:css -->', ''))
+    .pipe(replace('<!-- inject:js -->', ''))
+    .pipe(replace('<!-- inject:html -->', ''))
+    .pipe(replace('<!-- endinject -->', ''))
+    .pipe(removeEmptyLines({
+        removeComments: true
+    }))
+    .pipe(replace(new RegExp('<style>\n</style>', 'g'), ''))
+    .pipe(replace(new RegExp('<script type="text/javascript">\n</script>', 'g'), ''))
+    .pipe(removeEmptyLines({
+        removeComments: true
+    }))
+    .pipe(gulp.dest(parentDirPath));
+}
 
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
@@ -158,7 +197,7 @@ gulp.task('copy', function() {
 })
 
 // Run everything
-gulp.task('default', ['less-main', 'minify-css-main', 'minify-js-main', 'less-dashboard', 'minify-css-dashboard', 'minify-js-dashboard', 'copy']);
+gulp.task('default', ['compile-html-modules', 'less-main', 'minify-css-main', 'minify-js-main', 'less-dashboard', 'minify-css-dashboard', 'minify-js-dashboard', 'copy']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
@@ -170,14 +209,14 @@ gulp.task('browserSync', function() {
 });
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less-main', 'minify-css-main', 'minify-js-main', 'less-dashboard', 'minify-css-dashboard', 'minify-js-dashboard'], function() {
+gulp.task('dev', ['browserSync', 'compile-html-modules', 'less-main', 'minify-css-main', 'minify-js-main', 'less-dashboard', 'minify-css-dashboard', 'minify-js-dashboard'], function() {
     gulp.watch('less/*.less', ['less-main', 'less-dashboard']);
     gulp.watch('less/**/*.less', ['less-main', 'less-dashboard']);
     gulp.watch('css/*.css', ['minify-css-main', 'minify-css-dashboard']);
     gulp.watch('css/**/*.css', ['minify-css-main', 'minify-css-dashboard']);
     gulp.watch('js/*.js', ['minify-js-main', 'minify-js-dashboard']);
     gulp.watch('js/**/*.js', ['minify-js-main', 'minify-js-dashboard']);
-    // Reloads the browser whenever HTML or JS files change
+    // Reloads the browser whenever HTML or JS files change    
     gulp.watch('*.html', browserSync.reload);
     gulp.watch('templates/*.html', browserSync.reload);
     gulp.watch('templates/**/*.html', browserSync.reload);
@@ -186,4 +225,9 @@ gulp.task('dev', ['browserSync', 'less-main', 'minify-css-main', 'minify-js-main
     gulp.watch('less/*.less', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
     gulp.watch('js/*.js', browserSync.reload);
+    //Reload the browse on every HTML module change
+    watchHtmlDirForModuleChanges('templates/dashboard/pages')
+    watchHtmlDirForModuleChanges('templates/dashboard/partials')
+    watchHtmlDirForModuleChanges('templates/main/pages')
+    watchHtmlDirForModuleChanges('templates/main/partials')
 });
