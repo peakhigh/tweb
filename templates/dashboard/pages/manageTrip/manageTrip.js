1a3bd299b61ea26c9d77cacbf5bc996c835e126b
Bhavani Krishna <bkrishna@echoice.com.au>
2017-02-21 09:24:55 +1100

    Grid ui changes

--- templates/dashboard/pages/manageTrip/manageTrip.js
+++ templates/dashboard/pages/manageTrip/manageTrip.js
@@ -1,5 +1,20 @@
 // var pageName = 'manageTrip';
 console.log("Manage Trips");
+console.log('template data', UTILS.getCurrentTemplateData());
 // console.log(UTILS.getCurrentTemplateData());
 // console.log('....');
 // console.log(UTILS.getTemplateData(pageName));
+
+
+$(document).ready(function () {
+    // var source   = $("#entry-template").html();
+    // console.log(source);
+    // var template = Handlebars.compile(source);
+    // var context = {title: "My New Post", body: "This is my first post!"};
+    // var html    = template(context);
+    // console.log(html);
+});
+//get total from backend
+//grid plugin with header-template, data-template, data, paging options
+//may be grid rendering should be handle bars templates
+//should be like a grid plugin, not like a handle bars template
