f98ab56b59472d40626fd1aae0fd718b3cf53107
leelakrishna <klkrishna.cse@gmail.com>
2017-03-03 08:37:57 +0530

    Add driver,manage driver

---
+++ templates/dashboard/pages/addDriver/addDriver.js
@@ -0,0 +1,14 @@
+// console.log(JSON.stringify(UTILS.getCurrentTemplateData()));
+console.log('template data', UTILS.getCurrentTemplateData());
+
+$(document).ready(function () {
+    var config = UTILS.getCurrentTemplateData();
+    FORM_HELPER.draw(".new-driver-content", config, {
+        postUrl: "drivers/service/addDriver",
+        callbacks: {
+            afterSubmit: function () {
+                MENU_HELPER.menuClick('addDriver', 'addDriver');
+            }
+        },
+    });
+});
---
+++ templates/dashboard/pages/manageDriver/manageDriver.js
@@ -0,0 +1,15 @@
+// console.log(JSON.stringify(UTILS.getCurrentTemplateData()));
+console.log('template data', UTILS.getCurrentTemplateData());
+
+$(document).ready(function () {
+    var config = UTILS.getCurrentTemplateData();
+    console.log(config);
+    FORM_HELPER.draw(".manage-driver-content", config, {
+        postUrl: "drivers/service/manageDriver",
+        callbacks: {
+            afterSubmit: function () {
+                MENU_HELPER.menuClick('manageDriver', 'manageDriver');
+            }
+        },
+    });
+});
