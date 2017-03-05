5e6ed219666eddab5969f86c3765c4788e40cb93
leelakrishna <klkrishna.cse@gmail.com>
2017-03-05 14:58:14 +0530

    mangedriver modifications

--- templates/dashboard/pages/manageDriver/manageDriver.js
+++ templates/dashboard/pages/manageDriver/manageDriver.js
@@ -1,15 +1,11 @@
-// console.log(JSON.stringify(UTILS.getCurrentTemplateData()));
 console.log('template data', UTILS.getCurrentTemplateData());
-
 $(document).ready(function () {
-    var config = UTILS.getCurrentTemplateData();
-    console.log(config);
-    // FORM_HELPER.draw(".manage-driver-content", config, {
-    //     postUrl: "drivers/service/manageDriver",
-    //     callbacks: {
-    //         afterSubmit: function () {
-    //             MENU_HELPER.menuClick('manageDriver', 'manageDriver');
-    //         }
-    //     },
-    // });
+    var moduleData = UTILS.getCurrentTemplateData();
+    var grid = new GRID_HELPER.GRID('.manage-drivers-content', {
+        gridData: moduleData,
+        gridId: 'gridManageDrivers',
+        rowConfig: {            
+            template: 'grid-row-template-details'
+         }
+    });
 });
